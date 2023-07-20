b0c9eaafbbb42160904d5bb4c39b4484ebb03997
stormalex7 <135204348+stormalex7@users.noreply.github.com>
2023-07-20 17:32:05 +0800

    Add files via upload

---
+++ config/config.py
@@ -0,0 +1,9 @@
+import os
+
+PROJECT_NAME = 'merge'
+PROJECT_PATH = os.path.realpath(os.path.join('..', PROJECT_NAME))
+
+ORIGINAL_PATH = os.path.join(PROJECT_PATH, 'images/')
+TICK_PATH = os.path.join(PROJECT_PATH, 'ticks/')
+SIGN_PATH = os.path.join(PROJECT_PATH, 'signs/')
+
